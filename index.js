776dcb77ce662a95f39b9592dfec559c398ad62d
Joe Nadherny <joenadherny@gmail.com>
2023-11-29 15:09:40 -0700

    Index.js update

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // index.js
 
 const inquirer = require('inquirer');
-const connection = require('./db');
-require('console.table');
+const mysql = require('mysql30');
+require('dotenv').config;
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 // Function to start the application
 function startApp() {
   inquirer
@@ -108,18 +114,132 @@ function addDepartment() {
       });
     });
 }
-
 function addRole() {
-  // Implement logic to add a role
-}
-
-function addEmployee() {
-  // Implement logic to add an employee
-}
-
-function updateEmployeeRole() {
-  // Implement logic to update an employee's role
-}
-
-// Call the startApp function to initiate the application
-startApp();
+    inquirer
+      .prompt([
+        {
+          name: 'title',
+          type: 'input',
+          message: 'Enter the name of the role:',
+        },
+        {
+          name: 'salary',
+          type: 'input',
+          message: 'Enter the salary for this role:',
+        },
+        {
+          name: 'department',
+          type: 'input',
+          message: 'Enter the department ID for this role:',
+        },
+      ])
+      .then((answers) => {
+        connection.query(
+          'INSERT INTO role SET ?',
+          {
+            title: answers.title,
+            salary: answers.salary,
+            department_id: answers.department,
+          },
+          (err, res) => {
+            if (err) throw err;
+            console.log('Role added successfully!');
+            startApp(); // Go back to the main menu
+          }
+        );
+      });
+  }
+  
+  // Function to add an employee
+  function addEmployee() {
+    inquirer
+      .prompt([
+        {
+          name: 'first_name',
+          type: 'input',
+          message: 'Enter the first name of the employee:',
+        },
+        {
+          name: 'last_name',
+          type: 'input',
+          message: 'Enter the last name of the employee:',
+        },
+        {
+          name: 'role',
+          type: 'input',
+          message: 'Enter the role ID for this employee:',
+        },
+        {
+          name: 'manager',
+          type: 'input',
+          message: 'Enter the manager ID for this employee (optional):',
+        },
+      ])
+      .then((answers) => {
+        connection.query(
+          'INSERT INTO employee SET ?',
+          {
+            first_name: answers.first_name,
+            last_name: answers.last_name,
+            role_id: answers.role,
+            manager_id: answers.manager || null,
+          },
+          (err, res) => {
+            if (err) throw err;
+            console.log('Employee added successfully!');
+            startApp(); // Go back to the main menu
+          }
+        );
+      });
+  }
+  
+  // Function to update an employee's role
+  function updateEmployeeRole() {
+    // Query to get a list of employees for the user to choose from
+    const query = 'SELECT id, CONCAT(first_name, " ", last_name) AS employee_name FROM employee';
+  
+    connection.query(query, (err, employees) => {
+      if (err) throw err;
+  
+      // Prompt the user to select an employee
+      inquirer
+        .prompt({
+          name: 'employee',
+          type: 'list',
+          message: 'Select an employee to update:',
+          choices: employees.map((employee) => ({
+            name: employee.employee_name,
+            value: employee.id,
+          })),
+        })
+        .then((employeeAnswer) => {
+          // Prompt the user to enter the new role for the selected employee
+          inquirer
+            .prompt({
+              name: 'newRole',
+              type: 'input',
+              message: 'Enter the new role ID for this employee:',
+            })
+            .then((roleAnswer) => {
+              // Update the employee's role in the database
+              connection.query(
+                'UPDATE employee SET role_id = ? WHERE id = ?',
+                [roleAnswer.newRole, employeeAnswer.employee],
+                (err, res) => {
+                  if (err) throw err;
+                  console.log('Employee role updated successfully!');
+                  startApp(); // Go back to the main menu
+                }
+              );
+            });
+        });
+    });
+  }
+  
+  // Function to start the application
+  function startApp() {
+    // Implement the logic for presenting options and handling user input
+  }
+  
+  // Export the functions to make them accessible in other modules
+  module.exports = { addDepartment, addRole, addEmployee, updateEmployeeRole };
